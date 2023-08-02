6ef4e0982b72caff9d6a09081e3641148cf193da
Fernando Moreno Arroyo <76887786+FernandoAMrn@users.noreply.github.com>
2023-08-01 23:30:02 -0600

    BasicamenteTodo el juego

--- ER_VR/Assets/C# Scripts/HP_Bar.cs
+++ ER_VR/Assets/C# Scripts/HP_Bar.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HP_Bar : MonoBehaviour
 {
+    public Slider slider;
     // Start is called before the first frame update
-    void Start()
+    public void SetMaxHealth(int health)
     {
-        
+
+        slider.value = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
+        slider.value = health;
     }
 }
---
+++ ER_VR/Assets/C# Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public HP_Bar healthBar;
+
+    public GameObject victoryScreen;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void addHP()
+    {
+        currentHealth += 1;
+        healthBar.SetHealth(currentHealth);
+    }
+    public void Update()
+    {
+        if (currentHealth == 20)
+        {
+            victoryScreen.SetActive(true);
+        }
+    }
+
+
+}
